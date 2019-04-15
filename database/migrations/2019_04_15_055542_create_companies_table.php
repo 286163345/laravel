cd77cd54061fa2d90c3a4fc9b124aa9ab383556e
乔明明 <286163345@qq.com>
2019-04-15 19:17:12 +0800

    no message

---
+++ database/migrations/2019_04_15_055542_create_companies_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            //递增 ID（主键），相当于「UNSIGNED BIG INTEGER」
+            $table->bigIncrements('id');
+            //相当于带长度的 VARCHAR
+            $table->string('name')->default(null);
+            $table->string('uuid');
+            $table->dateTime('created_at')->default(null);
+            $table->dateTime('update_at')->default(null);
+            $table->unique('uuid');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
--- readme.md
+++ readme.md
@@ -1,3 +1,15 @@
 1.拉取项目后将 env.example复制一份改为 .env文件
 2.执行composer install下载vendor文件
-3.不想导入表 可以执行 php artisan admin:install生成新的表
+3.执行php artisan vendor:publish --provider="Encore\Admin\AdminServiceProvider"发布资源
+4.执行 php artisan admin:install 生成新的表
+生成appkey: php artisan key:generate  
+用到的命令:
+php artisan admin:make UserController --model=App\User  laraveladmin控制器创建
+php artisan make:migration create_company_table  创建迁移文件
+php artisan migrate   执行迁移
+php artisan migrate:rollback  执行回滚迁移(最后一次)
+php artisan migrate:reset   执行回滚迁移(所有)
+php artisan migrate:refresh  重置数据库并重新运行所有迁移
+
+php artisan make:model Brand -m　　//创建模型并生成迁移文件
+php artisan admin:make BrandController --model=App\Brand　　//创建关联Brand模型的控制器
