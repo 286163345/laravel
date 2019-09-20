928b73b8f1bb35543eefd7589a537358f18b6947
BED-qiao Ming Ming <286163345@qq.com>
2019-09-20 15:34:12 +0800

    注释路由请求方法

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -27,6 +27,9 @@ class CompanyController extends Controller
         return $this->renderView('blade.company.list',$param);
     }
 
+    /**
+     * 方法:GET 请求URL:/company/creat
+     */
     public function create(FormBuilder $formBuilder)
     {
         $form = $formBuilder->create(CompanyForm::class, [
@@ -41,6 +44,9 @@ class CompanyController extends Controller
         return view('blade.company.add',$param);
     }
 
+    /**
+     * 方法:POST 请求URL:/company
+     */
     public function store(FormBuilder $formBuilder)
     {
         $form = $formBuilder->create(CompanyForm::class);
@@ -51,5 +57,37 @@ class CompanyController extends Controller
         if(!empty($company->id)){
             return redirect('show/company')->with('message', '保存成功!');;
         }
+    }
+
+    /**
+     * 方法:GET 请求URL:/company/{id}
+     */
+    public function show()
+    {
+
+    }
+
+    /**
+     * 方法:GET 请求URL:/company/{id}/edit
+     */
+    public function edit()
+    {
+
+    }
+
+    /**
+     * 方法:PUT/PATCH  请求URL:/company/{id}
+     */
+    public function save()
+    {
+
+    }
+
+    /**
+     * 方法:GET  请求URL:/company/{id}
+     */
+    public function destroy()
+    {
+
     }
 }
